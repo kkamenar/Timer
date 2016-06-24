730328bea4fe35f2ebb5ea1b56bf4c9765ca09aa
Kristin Kamenar <kkamenar@kristins-macbook-air-5.local>
2016-06-24 11:34:00 -0500

    Timer with play, pause, and clear buttons. Displays amount of time passed.

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -9,12 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var countingLabel: UILabel!
+    
+    //create an instance of NSTimer
+    var SwiftTimer = NSTimer()
+    
+    //Display the swift tmer number to the user
+    var SwiftCounter = 0
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
 
+        countingLabel.text = String(SwiftCounter)
+    }
+    
+    @IBAction func startButton(sender: AnyObject) {
+        
+        //Variable to define number of seconds between firings of timer
+        let TIME_INCREMENT = 1.0
+        
+        
+        //run every 1 second; call a function called updateCounter; repeat
+        SwiftTimer = NSTimer.scheduledTimerWithTimeInterval(TIME_INCREMENT, target: self, selector: #selector(ViewController.updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    func updateCounter()
+    {
+        SwiftCounter+=1
+        countingLabel.text = String(SwiftCounter)
+        print(SwiftCounter)
+    }
+    
+
+    @IBAction func pauseButton(sender: AnyObject) {
+        
+        SwiftTimer.invalidate()
+    }
+    
+    @IBAction func clearButton(sender: AnyObject) {
+        
+        SwiftTimer.invalidate()
+        SwiftCounter = 0
+        countingLabel.text = String(SwiftCounter)
+        
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
